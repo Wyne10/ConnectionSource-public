8fe1186b2b074064918ef3f8adfd074ba7ec57c8
Wyne10 <izmodenov1997@gmail.com>
2025-07-23 07:46:54 +0500

    Add caution message

--- src/main/java/org/bigcraft/connection/command/ConnectionCommand.java
+++ src/main/java/org/bigcraft/connection/command/ConnectionCommand.java
@@ -5,15 +5,18 @@ import com.google.inject.Singleton;
 import dev.jorel.commandapi.CommandTree;
 import dev.jorel.commandapi.arguments.LiteralArgument;
 import org.bigcraft.connection.ConnectionSource;
+import org.bigcraft.connection.sql.ConnectionProvider;
 
 @Singleton
 public class ConnectionCommand {
 
     private final ConnectionSource plugin;
+    private final ConnectionProvider connectionProvider;
 
     @Inject
-    public ConnectionCommand(ConnectionSource plugin) {
+    public ConnectionCommand(ConnectionSource plugin, ConnectionProvider connectionProvider) {
         this.plugin = plugin;
+        this.connectionProvider = connectionProvider;
         registerCommand();
     }
 
@@ -22,9 +25,23 @@ public class ConnectionCommand {
                 .then(new LiteralArgument("reload")
                         .withPermission("connection.reload")
                         .executes((sender, args) -> {
-                            plugin.reload();
-                            plugin.getLog().info("Reloaded connection source");
-                        }))
+                            if (connectionProvider.isActive()) {
+                                sender.sendMessage("Connection source is already active.");
+                                sender.sendMessage("Are you sure you want to reload the connection source? It may cause data loss in dependant plugins.");
+                                sender.sendMessage("To confirm, type /connection reload confirm");
+                            } else {
+                                plugin.reload();
+                                sender.sendMessage("Reloaded connection source");
+                                plugin.getLog().info("Reloaded connection source");
+                            }
+                        })
+                        .then(new LiteralArgument("confirm")
+                                .withPermission("connection.reload")
+                                .executes((sender, args) -> {
+                                    plugin.reload();
+                                    sender.sendMessage("Reloaded connection source");
+                                    plugin.getLog().info("Reloaded connection source");
+                                })))
                 .register(plugin);
     }
 
